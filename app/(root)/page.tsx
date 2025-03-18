55b9c6bea61482f727e606c3f1ca104feeef00ca
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-18 22:23:10 +0500

    feat:home page section added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import Bell from "@/assets/icons/bell.svg"
 import user from "@/assets/images/user.png"
 import galochka from "@/assets/icons/galochka.svg"
+import xIcon from "@/assets/icons/xicon.svg"
+import thunder from "@/assets/icons/thunder.svg"
+import heart from "@/assets/icons/heart-home.svg"
+import weddingRingFill from "@/assets/icons/Wedding_Ring-fill.svg"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -51,6 +55,9 @@ export default function Home() {
             </div>
             <div className="absolute h-1/2  w-[342px] flex flex-col justify-center gap-2 px-[19px]  translate-x-1 bottom-0 user-content z-20 rounded-4xl text-white">
               <span className="text-xs tracking-[1px] text-[#BABABA]">Warszawa</span>
+              <button className="max-w-[95px] h-[26px] p-2 rounded-4xl flex items-center gap-1 button-gradient button-shadow font-bold text-[#F1EAEF] font-jakarta text-[8px] leading-[120%] align-middle">
+                <Image src={weddingRingFill} width={11} height={10} alt="" />
+                Bez  zobowiązań</button>
               <div className="flex gap-2 items-end">
                 <h2 className="text-2xl font-medium leading-[120%] font-jakarta">Mateusz, 25</h2>
                 <Image src={galochka} width={25} height={25} alt="" />
@@ -61,6 +68,24 @@ export default function Home() {
           </SwiperSlide>
           <SwiperSlide>Slide 3</SwiperSlide>
         </Swiper>
+
+        <div className="w-max mx-auto flex gap-4 mt-4" >
+          <div className="w-14 h-14 flex items-center justify-center rounded-full cursor-pointer" style={{
+            background: "linear-gradient(160.84deg, #2E92FF 12.89%, #7D9FC4 83.44%)"
+          }}>
+            <Image src={xIcon} alt="xicon" />
+          </div>
+          <div className="w-16 h-15 flex items-center justify-center rounded-full cursor-pointer" style={{
+            background: "linear-gradient(337.18deg, #FF63BB 4%, #8211C8 110.58%)"
+          }}>
+            <Image src={thunder} alt="thunder icon" />
+          </div>
+          <div className="w-14 h-14 flex items-center justify-center rounded-full cursor-pointer" style={{
+            background: "linear-gradient(166.45deg, #9652FF 9.71%, #BD95FA 94.97%)"
+          }}>
+            <Image src={heart} alt="heart icon" />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
