c3b2618eedaae1a52f07e0c39b62c9e386cf23c7
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-23 01:12:01 +0500

    test mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import ScreenCheck from "@/components/screen-check";
+// import ScreenCheck from "@/components/screen-check";
 
 export const metadata: Metadata = {
   title: "Buziak",
@@ -18,9 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        <ScreenCheck>
+        {/* <ScreenCheck> */}
           {children}
-        </ScreenCheck>
+        {/* </ScreenCheck> */}
       </body>
     </html>
   );
