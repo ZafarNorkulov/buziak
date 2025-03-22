07d7ea641c1648178c2976cf17d6aef98e426fb0
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-22 22:10:26 +0500

    styles: authLayout min-h-screen

--- app/auth/(auth-layout)/layout.tsx
+++ app/auth/(auth-layout)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='bg-[#1b1b1b] h-screen w-screen text-white pt-10 auth-layout'>
+        <div className='bg-[#1b1b1b] min-h-screen w-screen text-white pt-10 auth-layout'>
             <div className="max-container !h-full">
                 <SessionProvider>
 
