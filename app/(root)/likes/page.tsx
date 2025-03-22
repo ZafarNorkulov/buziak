014f6e6a6e9947c36e32b3b97fd44fcc0caea135
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-22 20:27:06 +0500

    change

--- app/(root)/likes/page.tsx
+++ app/(root)/likes/page.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import sectionBg from "@/assets/images/section-screen.png";
+import LikedComponent from '@/components/likes-components';
 
 
 
 
 const LikesPage = () => {
- 
+
     return (
         <section
             className="min-h-[calc(100vh-88px)] bg-top bg-no-repeat bg-cover pt-[14px]"
             style={{ backgroundImage: `url(${sectionBg.src})` }}
         >
             <div className="max-container">
-               
+                <LikedComponent />
             </div>
         </section>
 
