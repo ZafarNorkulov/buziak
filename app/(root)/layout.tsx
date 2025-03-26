2f3cd7f158de5b331149b853ec6aa8dc59ffc673
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-26 13:20:31 +0500

    styles: profile page heaight

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-[calc(100vh-88px)] pb-[88px] flex flex-col relative">
+    <div className="min-h-[calc(100vh-67px)] flex flex-col relative">
       <main className="flex-1">
         {children}
       </main>
